5ba54fdcf051eb83187567d00def6f81382cd884
Алиса <alice@hui.com>
2022-03-10 19:58:26 +0300

    добавил в renderComments цикл forEach

--- js/fullphoto.js
+++ js/fullphoto.js
@@ -1,23 +1,25 @@
-const bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
-
 /**
  *Отображает комментарии
- * @param {object} commentsData Случайный комментарий
+ * @param {Array} commentsData Случайный комментарий
  */
 const renderComments = (commentsData) => {
-  const commentBox = document.createElement('li');
-  commentBox.classList.add('social__comment');
-  const commenter = document.createElement('img');
-  commenter.classList.add('social__picture');
-  commenter.src = commentsData.comments.avatar;
-  commenter.alt = commentsData.comments.name;
-  commenter.width = 35;
-  commenter.height = 35;
-  commentBox.append(commenter);
-  const commentText = document.createElement('p');
-  commentText.classList.add('social__text');
-  commentText.textContent = commentsData.comments.message;
+  const commentsContainer = document.querySelector('.social__comments');
+  commentsContainer.innerHTML = '';
+  commentsData.forEach((element) => {
+    const commentBox = document.createElement('li');
+    commentBox.classList.add('social__comment');
+    const commenter = document.createElement('img');
+    commenter.classList.add('social__picture');
+    commenter.src = element.avatar;
+    commenter.alt = element.name;
+    commenter.width = 35;
+    commenter.height = 35;
+    commentBox.append(commenter);
+    const commentText = document.createElement('p');
+    commentText.classList.add('social__text');
+    commentText.textContent = element.message;
+  });
+
 };
 
 
@@ -26,6 +28,9 @@ const renderComments = (commentsData) => {
  * @param {object} photoData пост
  */
 const renderFullPhoto  = (photoData) => {
+  const bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+
   const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
   bigPictureImg.src = photoData.url;
 
@@ -36,26 +41,27 @@ const renderFullPhoto  = (photoData) => {
   commentCount.textContent = photoData.comments.length;
 
   const commentsContainer = document.querySelector('.social__comments');
-  commentsContainer.append(renderComments(photoData));
+  commentsContainer.append(renderComments(photoData.comments));
 
   const socialCaption = bigPicture.querySelector('.social__caption');
   socialCaption.textContent = photoData.description;
+
+  const closeButton = bigPicture.querySelector('.big-picture__cancel');
+  closeButton.addEventListener('click', () => {bigPicture.classList.add('hidden');});
+
+  document.addEventListener('keypress', (e) => {
+    if (e.key === 'Esc') {
+      bigPicture.classList.add('hidden');
+    }
+  });
+
+  const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+  const commentLoader = bigPicture.querySelector('.comments-loader');
+  socialCommentCount.classList.add('hidden');
+  commentLoader.classList.add('hidden');
+  const docBody = document.querySelector('body');
+  docBody.classList.add('modal-open');
 };
-
-
-const socialCommentCount = bigPicture.querySelector('.social__comment-count');
-const commentLoader = bigPicture.querySelector('.comments-loader');
-socialCommentCount.classList.add('hidden');
-commentLoader.classList.add('hidden');
-const docBody = document.querySelector('body');
-docBody.classList.add('modal-open');
-
-const closeButton = bigPicture.querySelector('.big-picture__cancel');
-closeButton.addEventListener('click', () => {bigPicture.classList.add('hidden');});
-
-closeButton.addEventListener('keypress', (e) => {
-  if(e.keyCode === 27) {bigPicture.classList.add('hidden');}
-});
 
 
 export{renderFullPhoto};
